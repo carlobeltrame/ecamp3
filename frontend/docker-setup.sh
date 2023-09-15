1df5114497a598f976b6bbdae3070beec0e9ef2d
Carlo Beltrame <carlo@beltra.me>
2023-09-15 15:33:18 +0200

    Fix unnecessary git changes when starting the project

--- frontend/docker-setup.sh
+++ frontend/docker-setup.sh
@@ -3,9 +3,16 @@ set -euo pipefail
 
 BASEDIR=$(dirname "$0")
 ENV_FILE=$BASEDIR"/public/environment.js"
+PDF_FILE=$BASEDIR"/src/pdf/pdf.mjs"
 
 if [ ! -f "$ENV_FILE" ]; then
     cp $BASEDIR/public/environment.docker.dist "$ENV_FILE"
+fi
+
+if [ ! -f "$PDF_FILE" ]; then
+    # Copy dummy versions of the pdf build outputs, to make sure there is always something to import
+    cp $BASEDIR/src/pdf/pdf.mjs.dist "$PDF_FILE"
+    cp $BASEDIR/src/pdf/prepareInMainThread.mjs.dist "$BASEDIR/src/pdf/prepareInMainThread.mjs"
 fi
 
 if [ "$CI" = 'true' ] ; then
